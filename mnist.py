b34da2620eb0f9515b89adf8d67208a62298446b
Xu Kui <xukui@sensetime.com>
2017-12-20 21:28:37 +0800

    add code

--- mnist.py
+++ mnist.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import os
 import argparse
 import torch
 import torch.nn as nn
@@ -6,14 +7,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
-
+from torchnet.logger import MeterLogger
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--epochs', type=int, default=50, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
@@ -23,8 +24,10 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='how many batches to wait before logging training status')
+parser.add_argument('--port', type=int, default=9999, metavar='N',
+                    help='port that visdom service on (default: 9999)')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -32,6 +35,9 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
+# online ploter
+os.system("python -m visdom.server -port "+str(args.port)+" &")
+mlog = MeterLogger(port=args.port, nclass=10,title="mnist_ConvNet")
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
@@ -76,6 +82,8 @@ optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 def train(epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
+	# online ploter
+	mlog.meter['time'].reset()
         if args.cuda:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data), Variable(target)
@@ -84,28 +92,34 @@ def train(epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+	# online ploter
+	mlog.updateMeter(output, target, loss)
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+	    # online ploter
+	    mlog.printMeter("Train", epoch, batch_idx, len(train_loader))  
+    # online ploter
+    mlog.resetMeter(epoch, mode='Train')
 
 def test():
     model.eval()
     test_loss = 0
     correct = 0
-    for data, target in test_loader:
+    for batch_idx, (data, target) in enumerate(test_loader):
+	# online ploter
+	mlog.meter['time'].reset()
         if args.cuda:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        test_loss += F.nll_loss(output, target) 
+	# online ploter
+	mlog.updateMeter(output, target, test_loss)
+        if batch_idx % args.log_interval == 0:
+	    # online ploter
+	    mlog.printMeter("Test", epoch, batch_idx, len(test_loader))  
 
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    # online ploter
+    mlog.resetMeter(epoch, mode='Test')
 
 
 for epoch in range(1, args.epochs + 1):
